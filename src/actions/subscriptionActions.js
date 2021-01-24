a0994d7ee2309c61207079153fe2395820cc6698
jovaslink <jovaslink@gmail.com>
2021-01-23 19:09:05 -0600

    First commit

--- src/actions/subscriptionActions.js
+++ src/actions/subscriptionActions.js
@@ -227,7 +227,6 @@ export const retryInvoiceWithNewPaymentMethod = (
             Swal.fire("ERROR", error.message, "error");
           });
 
-        //dispatch(isRetrySubscription(true));
         dispatch(finishLoading());
       });
   };
